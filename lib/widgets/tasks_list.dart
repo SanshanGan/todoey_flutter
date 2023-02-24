c73af4ebc9998e9b948aaf08cfc4827ff34b59d3
sanshan.gan <lucagsscd@gmail.com>
2023-02-24 16:36:50 +0800

    [sanshan]feat: create an empty task list and change the widget into statefulWidget

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'task_tile.dart';
+import 'package:mytodo/models/task.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
   const TasksList({
     super.key,
   });
 
   @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  @override
   Widget build(BuildContext context) {
+
+    List<Task> tasks = [];
     return ListView(
       children: [
         TaskTile(),
