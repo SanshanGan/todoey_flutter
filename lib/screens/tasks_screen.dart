7c15437aa724835b0ec6cfeb46380eaadd540eb2
sanshan.gan <lucagsscd@gmail.com>
2023-02-24 11:03:18 +0800

    [sanshan]refactor: extract the listTile as a new widget named TaskTile

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -77,25 +77,29 @@ class TasksList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        ListTile(
-          title: Text('I am a task list.'),
-          trailing: Checkbox(
-            value: false,
-            onChanged: (bool? value) {
-              true;
-            },
-          ),
-        ),
-        ListTile(
-          title: Text('I am a task list.'),
-          trailing: Checkbox(
-            value: false,
-            onChanged: (bool? value) {
-              true;
-            },
-          ),
-        )
+        TaskTile(),
+        TaskTile(),
+        TaskTile(),
       ],
+    );
+  }
+}
+
+class TaskTile extends StatelessWidget {
+  const TaskTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text('I am a task list.'),
+      trailing: Checkbox(
+        value: false,
+        onChanged: (bool? value) {
+          true;
+        },
+      ),
     );
   }
 }
