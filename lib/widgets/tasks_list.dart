61fc0401352601ca31f345b1c8e92b9a28e8028b
sanshan.gan <lucagsscd@gmail.com>
2023-02-24 17:23:28 +0800

    [sanshan]feat: use listview builder to create a general way to display task list

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -12,16 +12,22 @@ class TasksList extends StatefulWidget {
 }
 
 class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: 'Buy food'),
+    Task(name: 'Buy soap'),
+    Task(name: 'Buy ticket'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-
-    List<Task> tasks = [];
-    return ListView(
-      children: [
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTile(
+          taskTitle: tasks[index].name,
+          isChecked: tasks[index].isDone,
+        );
+      },
+      itemCount: tasks.length,
     );
   }
 }
