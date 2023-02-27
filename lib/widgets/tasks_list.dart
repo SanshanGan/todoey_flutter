801e9d35ee1a83d9f4ab8ccf45b255496af6c1f6
sanshan.gan <lucagsscd@gmail.com>
2023-02-27 17:53:48 +0800

    [sanshan]feat: add consumer for tasklist to use taskData widely

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -7,18 +7,22 @@ class TasksList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return TaskTile(
-            taskTitle: Provider.of<TaskData>(context).tasks[index].name,
-            isChecked: Provider.of<TaskData>(context).tasks[index].isDone,
-            checkboxCallback: (bool? checkboxState) {
-              // setState(() {
-              //   Provider.of<TaskData>(context).tasks[index].toggleDone();
-              // });
-            });
-      },
-      itemCount: Provider.of<TaskData>(context).tasks.length,
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return  ListView.builder(
+          itemBuilder: (context, index) {
+            return TaskTile(
+                taskTitle: taskData.tasks[index].name,
+                isChecked: taskData.tasks[index].isDone,
+                checkboxCallback: (bool? checkboxState) {
+                  // setState(() {
+                  //   Provider.of<TaskData>(context).tasks[index].toggleDone();
+                  // });
+                });
+          },
+          itemCount: taskData.tasks.length,
+        );
+      }
     );
   }
 }
